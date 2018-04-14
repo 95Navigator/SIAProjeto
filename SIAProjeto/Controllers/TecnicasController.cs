8c3d4a6944a593d3c154a2cf282d9ca10a2b75d8
Miguel Morais <deepmaikel@hotmail.com>
2018-04-14 15:26:18 +0100

    <TecnicasController> Adicionado

---
+++ SIAProjeto/Controllers/TecnicasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIAProjeto.Controllers
+{
+    public class TecnicasController : Controller
+    {
+        // GET: Tecnicas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
